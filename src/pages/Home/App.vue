f10b85539ffea3e2884e687a2a6cea7080a9d0e1
mcdonam7 <mcdonam7@tcd.ie>
2020-02-17 19:30:18 +0000

    app is now an MPA

--- src/pages/Home/App.vue
+++ src/pages/Home/App.vue
@@ -1,14 +1,45 @@
+<!-- Home App.vue -->
+
+
+
 <template>
   <div id="app">
+    
+    <img
+      alt="hand pic"
+      src="@/assets/hand.png"
+    >
+
+
     <title>A Helping Hand</title>
     <h1>A Helping Hand</h1>
+
+  
+
+  <nav>
+    <ul>
+      <li>Go to</li>
+
+      <li>
+        <a
+          href="about.html"
+        >
+          About Us
+        </a>
+       </li>
+    </ul>
+  </nav>
   </div>
+
 </template>
+
+
+
 
 <script>
 
 export default {
-  name: 'app',
+  name: 'Home',
   components: {
 
   }
