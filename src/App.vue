ab27d9a3cc5a77fa60c9b08f0675bf39e6d8b3d9
Chihun Lee <Chihun@live.ie>
2020-03-10 20:17:38 +0000

    Added nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,46 @@
 <template>
   <div id="app">
-       <router-view/>
+    
+    <b-navbar fixed-top="true" type="is-black" shadow="true">
+      <template slot="brand">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        </b-navbar-item>
+        </template>
+        <template slot="start">
+            <b-navbar-item href="/">
+                Home
+            </b-navbar-item>
+            <b-navbar-item href="gallery">
+                Hand Gallery
+            </b-navbar-item>
+            <b-navbar-item href="analyze">
+                Analyze
+            </b-navbar-item>
+            <b-navbar-dropdown label="Info">
+                <b-navbar-item href="about">
+                  About Us
+                </b-navbar-item>
+                <b-navbar-item href="blog">
+                  Blog
+                </b-navbar-item>
+            </b-navbar-dropdown>
+        </template>
+
+        <!--template slot="end">
+            <b-navbar-item tag="div">
+                <div class="buttons">
+<a class="button is-primary">
+    <strong>Sign up</strong>
+</a>
+<a class="button is-light">
+    Log in
+</a>
+                </div>
+            </b-navbar-item>
+        </template-->
+    </b-navbar>
+  
+  <router-view/>
   </div>
 </template>
 
