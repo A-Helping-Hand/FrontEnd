3cde90e82a0eb47a77e2f5196ed473e7b06972f8
Luan Williams <willialu@tcd.ie>
2020-03-12 11:03:09 +0000

    Updated meet the team page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -18,6 +18,130 @@
     <br>
     <p style="font-size:30px">MEET THE TEAM: </p>
 
+
+    <div class="row">
+      <div class="column">
+        <div class="card">
+          <img src="@/assets/luan.jpg" alt="Luan" style="width:20%">
+          <div class="container">
+          <h2>Luan Williams</h2>
+          <p class="title">CEO &amp; Founder</p>
+          <p>Leader, Master, Head Huncho, whatever you want to call me</p>
+          <p>example@example.com</p>
+          <p><button class="button">Contact</button></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="column">
+      <div class="card">
+        <img src="@/assets/paolo.jpg" alt="Paolo" style="width:20%">
+          <div class="container">
+          <h2>Paolo Moloney</h2>
+          <p class="title">Mr. Visionary</p>
+          <p>He is the man that can see all. Perfect 2020 Vision.</p>
+          <p>paolo@trinity.com</p>
+          <p><button class="button">Contact</button></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="column">
+      <div class="card">
+        <img src="@/assets/Anporo.jpg" alt="Anporo" style="width:30%">
+          <div class="container">
+          <h2>Anporo Zamora Mogollo</h2>
+          <p class="title">Madam Video</p>
+          <p>Anporo is an expert of videos, and makes for a perfect voice over</p>
+          <p>anporo@tcd.com</p>
+          <p><button class="button">Contact</button></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="column">
+      <div class="card">
+        <img src="@/assets/Alex.jpg" alt="Alex" style="width:15%">
+          <div class="container">
+          <h2>Alex Fleming</h2>
+          <p class="title">Biomedical Leader</p>
+          <p>Alex is in charge of the Biomedical Engineers. She is both a leader and a follower</p>
+          <p>alex@tcd.com</p>
+          <p><button class="button">Contact</button></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="column">
+      <div class="card">
+        <img src="@/assets/Matthew.jpg" alt="Matthew" style="width:11%">
+          <div class="container">
+          <h2>Matthew McCullen</h2>
+          <p class="title">Scrum Master</p>
+          <p>Loves getting knee-deep in a Good Scrum.</p>
+          <p>matthew@tcd.com</p>
+          <p><button class="button">Contact</button></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="column">
+      <div class="card">
+        <img src="@/assets/ryan.jpg" alt="ryan" style="width:20%">
+          <div class="container">
+          <h2>Ryan Hosn</h2>
+          <p class="title">The Artist</p>
+          <p>He is the modern day Picasso, Michaelangelo, Da Vinci, and Pat Kenny all in one</p>
+          <p>ryan@tcd.com</p>
+          <p><button class="button">Contact</button></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="column">
+      <div class="card">
+        <img src="@/assets/chihun.jpg" alt="chihun" style="width:20%">
+          <div class="container">
+          <h2>Chihun Lee</h2>
+          <p class="title">The Back End</p>
+          <p>A master of all back-ends</p>
+          <p>chihun@tcd.com</p>
+          <p><button class="button">Contact</button></p>
+        </div>
+      </div>
+    </div>
+
+  <div class="column">
+      <div class="card">
+        <img src="@/assets/luan&marek.jpg" alt="marek" style="width:15%">
+          <div class="container">
+          <h2>Marek Betka</h2>
+          <p class="title">Server Guy</p>
+          <p>Makes sure everything exists, and makes sense.</p>
+          <p>marek@tcd.com</p>
+          <p><button class="button">Contact</button></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="column">
+      <div class="card">
+        <img src="@/assets/Mary.jpg" alt="Mary" style="width:20%">
+          <div class="container">
+          <h2>Mary McDonald</h2>
+          <p class="title">Ms. Front-End</p>
+          <p>The boss of the front, doesn't like quitters.</p>
+          <p>mary@tcd.com</p>
+          <p><button class="button">Contact</button></p>
+        </div>
+      </div>
+    </div>
+
+
+   
+
+
+  </div>
  
   
 
@@ -62,5 +186,56 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.column {
+    float: left;
+    width: 50%;
+    margin-bottom: 100px;
+    padding: 0 0px;
+  }
+
+  /* Display the columns below each other instead of side by side on small screens */
+@media screen and (max-width: 650px) {
+  .column {
+    width: 100%;
+    display: block;
+  }
+
+  /* Add some shadows to create a card effect */
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
+
+/* Some left and right padding inside the container */
+.container {
+  padding: 0 16px;
+}
+
+/* Clear floats */
+.container::after, .row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.title {
+  color: grey;
+}
+
+.button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+}
+
+.button:hover {
+  background-color: #555;
+}
 }
 </style>
